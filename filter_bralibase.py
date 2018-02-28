16de16eb03299c37bb51f44956701926d5f868f2
Eugenio Mattei <emattei.phd@gmail.com>
2018-02-28 16:24:59 -0500

    - added the reference file to output

--- filter_bralibase.py
+++ filter_bralibase.py
@@ -8,31 +8,34 @@ import gzip
 import pathlib
 import re
 import glob
+import shutil
 
 
 def eprint(*args, **kwargs):
-	# http://stackoverflow.com/questions/5574702/
-	print(*args, file=sys.stderr, **kwargs)
+    # http://stackoverflow.com/questions/5574702/
+    print(*args, file=sys.stderr, **kwargs)
+
 
 ss_cons = {}
 
-fastq_fnp = sys.argv[1] # path to folded .fastq extension
-#for i in `ls *.folded`;do awk 'BEGIN{c=1}{if(c==1){print "@"substr($1,2);c+=1}else if(c==2){print $1;c+=1}else{print "+";print$1;c=1}} ' ${i} > ${i%%.folded}.fastq;done
+fastq_fnp = sys.argv[1]  # path to folded .fastq extension
+# for i in `ls *.folded`;do awk 'BEGIN{c=1}{if(c==1){print "@"substr($1,2);c+=1}else if(c==2){print $1;c+=1}else{print "+";print$1;c=1}} ' ${i} > ${i%%.folded}.fastq;done
 
 
 files = glob.glob(fastq_fnp + "/*.fastq")
 
 for f in files:
-	with open(f, "r") as handle:
-		for record in SeqIO.parse(handle, "fastq"):
-			ss_cons[record.id.replace("/","_")] = "".join([chr(c+33) for c in record.letter_annotations["phred_quality"]])
+    with open(f, "r") as handle:
+        for record in SeqIO.parse(handle, "fastq"):
+            ss_cons[record.id.replace("/", "_")] = "".join([chr(c+33)
+                                                            for c in record.letter_annotations["phred_quality"]])
 
 
-#bralibase folder containing k2, k3, ..., k15
+# bralibase folder containing k2, k3, ..., k15
 
 bralibase_fnp = sys.argv[2]
 
-#output folder
+# output folder
 
 outfolder = sys.argv[3]
 
@@ -40,23 +43,25 @@ files = glob.glob(bralibase_fnp + "/**/*.raw.fa", recursive=True)
 
 keys = set(ss_cons.keys())
 for f in files:
-	with open(f, "r") as handle:
-		tmp={}
-		for record in SeqIO.parse(handle, "fasta"):
-			tmp[record.id]=record.seq
-		if set(tmp.keys()) < keys:
-			outfile=os.path.join(outfolder,f.replace(bralibase_fnp,""))
-			
-			if not os.path.exists(os.path.dirname(outfile)):
-				try:
-					os.makedirs(os.path.dirname(outfile))
-				except OSError as exc: # Guard against race condition
-					if exc.errno != errno.EEXIST:
-						raise
-			
-			
-			with open(outfile,"w") as wh:
-				for k,v in tmp.items():
-					wh.write(">"+k+"\n"+str(v)+"\n"+ss_cons[k]+"\n")
+    with open(f, "r") as handle:
+        tmp = {}
+        for record in SeqIO.parse(handle, "fasta"):
+            tmp[record.id] = record.seq
 
+        if set(tmp.keys()) < keys:
+            outfile = os.path.join(outfolder, f.replace(bralibase_fnp, ""))
 
+            if not os.path.exists(os.path.dirname(outfile)):
+                try:
+                    os.makedirs(os.path.dirname(outfile))
+                except OSError as exc:  # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        raise
+
+            #copy the reference file to the destination
+            destination=outfile[:-7]+".ref.fa"
+            shutil.copy(f,destination)
+
+            with open(outfile, "w") as wh:
+                for k, v in tmp.items():
+                    wh.write(">"+k+"\n"+str(v)+"\n"+ss_cons[k]+"\n")
